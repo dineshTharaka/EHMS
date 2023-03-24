8041a35df5ae1bb00337c25a42de03294e66ed12
Buddhika <buddhikadilupulr@gmail.com>
2023-03-24 12:11:07 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import PatientRegister from "./pages/patientRegister";
+import Login from "./pages/Login";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <PatientRegister />
+      <Login/>
     </div>
   );
 }
